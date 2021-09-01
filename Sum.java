60b355317f6dfc7016d6165d5a2f468c3588d05e
Pavan134 <73532489+Pavan134@users.noreply.github.com>
2021-09-02 00:19:35 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,6 @@
+class Sum{
+	public static void main(String args[]){
+	  System.out.println("Hello java");
+	  int num1=5,num2=15,sum;
+	  sum=num1+num2;
+	  System.out.println("Sum of these numbers:"+sum);
